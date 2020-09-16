e0c3f8ca69113f89ca57f411ad419b019016a8ef
Giovani Zaghi <giovanizaghinogueira@outlook.com>
2020-09-16 15:36:59 -0300

    Separação em componentes

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,36 @@
+import 'package:despesas_flutter/components/transaction_form.dart';
+import 'package:despesas_flutter/components/transaction_list.dart';
+import 'package:despesas_flutter/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 210.28,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
